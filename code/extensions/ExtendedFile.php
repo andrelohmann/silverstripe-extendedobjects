62b309d70532f228dab4ccd706d52a14392c128a
andrelohmann <lohmann.andre@gmail.com>
2015-07-01 17:21:15 +0200

    tidyup of load function

--- code/extensions/ExtendedFile.php
+++ code/extensions/ExtendedFile.php
@@ -20,31 +20,14 @@ class ExtendedFile extends DataExtension {
 				
 		if(!$folderPath) $folderPath = Config::inst()->get('Upload', 'uploads_folder');
 		
-		//if(!$this->file) $this->file = new File();
-		
-		//if(!is_array($tmpFile)) {
-		//	user_error("Upload::load() Not passed an array.  Most likely, the form hasn't got the right enctype", E_USER_ERROR);
-		//}
-		
-		//$valid = $this->validate($tmpFile);
-		//if(!$valid) return false;
-		
 		// @TODO This puts a HUGE limitation on files especially when lots
 		// have been uploaded.
 		$base = Director::baseFolder();
 		$parentFolder = Folder::find_or_make($folderPath);
 
-		// Create a folder for uploading.
-		if(!file_exists(ASSETS_PATH)){
-			mkdir(ASSETS_PATH, Config::inst()->get('Filesystem', 'folder_create_mask'));
-		}
-		if(!file_exists(ASSETS_PATH . "/" . $folderPath)){
-			mkdir(ASSETS_PATH . "/" . $folderPath, Config::inst()->get('Filesystem', 'folder_create_mask'));
-		}
-
 		// Generate default filename
-                $fileArray = explode('/', $tmpFile);
-                $fileName = $fileArray[(count($fileArray)-1)];
+        $fileArray = explode('/', $tmpFile);
+        $fileName = $fileArray[(count($fileArray)-1)];
 		$nameFilter = FileNameFilter::create();
 		$file = $nameFilter->filter($fileName);
 		$fileName = basename($file);
@@ -70,8 +53,8 @@ class ExtendedFile extends DataExtension {
 				user_error("Couldn't fix $relativeFilePath with $i tries", E_USER_ERROR);
 			}
 		}
-
-		if(file_exists($tmpFile) && copy($tmpFile, "$base/$relativeFilePath")) {
+		
+		if(file_exists($tmpFile) && copy($tmpFile, $base."/".$relativeFilePath)) {
 			$this->owner->ParentID = $parentFolder->ID;
 			// This is to prevent it from trying to rename the file
 			$this->owner->Name = basename($relativeFilePath);
